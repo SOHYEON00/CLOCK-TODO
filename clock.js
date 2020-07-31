b50341adbfeae04cc55975e3bf028a249eb1e4be
SOHYEON00 <sohyeon0amy@gmail.com>
2020-07-31 05:38:42 +0100

    CLOCK 시간표현 디자인 변경 - 링크로 변경함 - 현재, LONDON만 연결 가능 (다른 도시 입력해도 LONDON시간 뜸) - 페이지 reload 버튼을 만들고자 했으나 조금 더 방안 고안중 - function 세분화 (요소 display )

--- clock.js
+++ clock.js
@@ -1,10 +1,36 @@
 const formClock = document.querySelector("#formClock");
 const inputPlace = formClock.querySelector("#inputPlace");
-const currentTime = document.querySelector("#nowTime");
+const currentTime = document.querySelector(".divClock");
 const greeting = formClock.querySelector(".greeting");
+
+
+// function clearClock() {
+// 	const button = document.querySelector("button");
+// 	notShowElements(button);
+// 	notShowElements(greeting);
+// 	notShowElements(currentTime);
+// 	showElements(inputPlace);
+// }
+
+// function addBtn(element) {
+// 	const reBtn = document.createElement('button');
+// 	element.appendChild(reBtn);
+// 	reBtn.innerHTML = "RESTART";
+// 	reBtn.addEventListener("click", clearClock);
+// }
 
 function savePlace(place) {
 	localStorage.setItem("place",place);
+}
+
+function showElements(element) {
+	element.classList.remove("notShowing");
+	element.classList.add("showing");
+}
+
+function notShowElements(element) {
+	element.classList.remove("showing");
+	element.classList.add("notShowing");
 }
 
 function getTime() {
@@ -13,20 +39,18 @@ function getTime() {
 	const mins = now.getMinutes();
 	const sec = now.getSeconds();
 
-	currentTime.classList.remove("notShowing");
-	currentTime.classList.add("showing");
-
-	currentTime.innerText = `${inputPlace.value} >> ${hours < 10 ? `0${hours}` : hours} : ${mins < 10 ? `0${mins}` : mins} : ${sec < 10 ? `0${sec}` : sec}`;
-
-}
+	showElements(currentTime);
+	//currentTime.innerText = `${inputPlace.value} >> ${hours < 10 ? `0${hours}` : hours} : ${mins < 10 ? `0${mins}` : mins} : ${sec < 10 ? `0${sec}` : sec}`;
+}	
 
 function getPlace(event) {
 	event.preventDefault();
 	const currentPlace = inputPlace.value;
-
-	inputPlace.classList.add("notShowing");
+	
 	greeting.innerText = `Hello, ${currentPlace}`;
-	greeting.classList.remove("notShowing");
+	notShowElements(inputPlace);
+	showElements(greeting);
+	//addBtn(greeting);
 
 	if(currentPlace !== null){
 		setInterval(getTime, 1000);
