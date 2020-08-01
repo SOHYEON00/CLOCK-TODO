6a900173134bbf7a7d2235830925be46f94760be
SOHYEON00 <sohyeon0amy@gmail.com>
2020-08-01 17:10:34 +0100

    CLOCK 기능 완료 - 지역(continent)과 도시(city)를 입력할 input 추가 - 입력받은 지역과 도시를 기반으로 해당 도시의 시각을 출력할 링크 생성 - 링크 생성: func findLink() - 오브젝트 관련 메소드 및 로직 연습하다 겹치는 변수 발견, 수정(todo.js, clock.js) - input 값 중 하나라도 입력되지 않았을 경우 alert

--- clock.js
+++ clock.js
@@ -1,7 +1,16 @@
 const formClock = document.querySelector("#formClock");
-const inputPlace = formClock.querySelector("#inputPlace");
+const inputContinent = formClock.querySelector("#inputContinent");
+const inputCity = formClock.querySelector("#inputCity");
+const greeting = document.querySelector(".greeting");
+const submitBtn = formClock.querySelector("#submitBtn");
 const currentTime = document.querySelector(".divClock");
-const greeting = formClock.querySelector(".greeting");
+const currentLink = currentTime.querySelector("iframe");
+
+
+let arrClock = [];
+const baseLink = "https://www.zeitverschiebung.net/clock-widget-iframe-v2?language=en&size=medium&timezone=";
+const spaceBar = "%2F";
+
 
 
 // function clearClock() {
@@ -19,8 +28,11 @@ const greeting = formClock.querySelector(".greeting");
 // 	reBtn.addEventListener("click", clearClock);
 // }
 
+
 function savePlace(place) {
-	localStorage.setItem("place",place);
+	arrClock.push(place);
+	let strbj = JSON.stringify(place);
+	localStorage.setItem("place",strbj);
 }
 
 function showElements(element) {
@@ -33,30 +45,47 @@ function notShowElements(element) {
 	element.classList.add("notShowing");
 }
 
-function getTime() {
-	const now = new Date();
-	const hours = now.getHours();
-	const mins = now.getMinutes();
-	const sec = now.getSeconds();
+function findLink(continent, city) {
+	let link = `${baseLink}${continent}${spaceBar}${city}`;
+	currentLink.src = link;
+}
 
-	showElements(currentTime);
+function getTime() {
+	// const now = new Date();
+	// const hours = now.getHours();
+	// const mins = now.getMinutes();
+	// const sec = now.getSeconds();
 	//currentTime.innerText = `${inputPlace.value} >> ${hours < 10 ? `0${hours}` : hours} : ${mins < 10 ? `0${mins}` : mins} : ${sec < 10 ? `0${sec}` : sec}`;
+	// let placeObj = {
+	// 	continent: continent,
+	// 	city: city
+	// };
+
+	// savePlace(placeObj);
+	showElements(currentTime);
 }	
 
 function getPlace(event) {
 	event.preventDefault();
-	const currentPlace = inputPlace.value;
-	
-	greeting.innerText = `Hello, ${currentPlace}`;
-	notShowElements(inputPlace);
-	showElements(greeting);
-	//addBtn(greeting);
+	let currentContinent = inputContinent.value;
+	let currentCity = inputCity.value;
+		
+	//객체로 저장하고 싶었는데, 뭔가 어렵다... 사실 굳이 객체가 필요없는 곳인 것 같음.
 
-	if(currentPlace !== null){
+	if( (currentContinent !== "") && (currentCity !== "" )){
+		greeting.innerText = `Hello, ${currentCity}`;
+		notShowElements(inputContinent);
+		notShowElements(inputCity);
+		notShowElements(submitBtn);
+		showElements(greeting);
+		findLink(currentContinent, currentCity);
 		setInterval(getTime, 1000);
-		savePlace(currentPlace);
+	}
+	else {
+		alert("Please re-write");
 	}
 }
+
 function init() {
 	formClock.addEventListener("submit", getPlace);
 }
--- todo.js
+++ todo.js
@@ -1,14 +1,24 @@
 const formTodo = document.querySelector(".conTodo .js-form");
 const inputTodos = formTodo.querySelector("#inputTodos");
 const listTodo = formTodo.querySelector("ul");
+const arrObj = [];
 
+const TODO_LS = "todo";
+
+function createObj(text) {
+	const todoObj = {
+		text: text,
+		id: arrObj.length + 1
+	};
+	arrObj.push(todoObj);
+	console.log(arrObj);
+}
 
 function onList() {
-	const savedTodo = localStorage.getItem("todo");
+	const savedTodo = localStorage.getItem(TODO_LS);
 	const newLi = document.createElement("li");
 	listTodo.appendChild(newLi);
 	newLi.innerText = savedTodo;
-	
 }
 
 function clearInput(target) {
@@ -18,8 +28,9 @@ function clearInput(target) {
 }
 
 function saveTodo(text) {
-	localStorage.setItem("todo", text);
+	localStorage.setItem(TODO_LS, text);
 	onList();
+	createObj(text);
 }
 
 function submitHandler(event) {
